5421069444ad19fafd2322056a1e5a9e36ba196c
Hector Nicolas Atencio <nicolasatencio90@gmail.com>
2023-05-29 20:09:08 -0300

    Terminando proyecto solo falta poder eliminar tareas

--- app.mjs
+++ app.mjs
@@ -1,2 +1,138 @@
-import inquirer from 'inquirer';
-console.log('Todo funciona normal')
+import inquirer from "inquirer";
+import listaDeTareas from "./lista_tareas.json" assert { type: "json" };
+import fs from "fs";
+import chalk from "chalk";
+
+const mostrarListaDeTareas = () => {
+  if (listaDeTareas.length < 1) return console.log("No hay tareas pendientes");
+  const opciones = listaDeTareas.map((tarea, index) => ({
+    name: `${index + 1}.${tarea.nombre}`,
+    value: tarea,
+  }));
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "tareaSeleccionada",
+        message: "Lista de tareas",
+        choices: opciones,
+      },
+    ])
+    .then((respuesta) => {
+      console.log("Tarea seleccionada:", respuesta.tareaSeleccionada.nombre);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const agregarTarea = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "nombre",
+        message: "Ingrese nombre de la tarea.",
+      },
+      {
+        type: "input",
+        name: "descripcion",
+        message: "Ingrese una descripcion de la tarea:",
+      },
+    ])
+    .then((respuesta) => {
+      const nuevaTarea = {
+        nombre: respuesta.nombre,
+        descripcion: respuesta.descripcion,
+        completada: false,
+      };
+      listaDeTareas.push(nuevaTarea);
+      fs.writeFile(
+        "./lista_tareas.json",
+        JSON.stringify(listaDeTareas),
+        "utf8",
+        (err) => {
+          if (err) console.log("Hubo un error al guardar los datos", err);
+          else console.log("Tarea guardada con exito");
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const menuPrincipal = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "opcion",
+        message: "¿Que desea hacer?",
+        choices: [
+          "Mostrar lista",
+          "Agregar tarea",
+          "Marcar como realizada",
+          "Salir",
+        ],
+      },
+    ])
+    .then((respuesta) => {
+      switch (respuesta.opcion) {
+        case "Mostrar lista":
+          mostrarListaDeTareas();
+          break;
+        case "Agregar tarea":
+          agregarTarea();
+          break;
+        case "Marcar como realizada":
+          marcarTarea();
+          break;
+        case "Salir":
+          console.log("Hasta luego");
+          break;
+        default:
+          console.log("Opcion invalida");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const marcarTarea = () => {
+  const opciones = listaDeTareas.map((tarea, index) => ({
+    name: `${index + 1}.${tarea.nombre}`,
+    value: tarea,
+  }));
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "echo",
+        message: "Marque la tarea realizada",
+        choices: opciones,
+      },
+    ])
+    .then((respuesta) => {
+      for (let i = 0; i < listaDeTareas.length; i++) {
+        if (listaDeTareas[i].nombre === respuesta.echo.nombre && listaDeTareas[i].descripcion === respuesta.echo.descripcion)
+          listaDeTareas[i].completada = true;
+      }
+      fs.writeFile(
+        "./lista_tareas.json",
+        JSON.stringify(listaDeTareas),
+        "utf8",
+        (err) => {
+          if (err) console.log(err);
+          else {
+            console.log("Tarea realizada:", chalk.green(respuesta.echo.nombre));
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+menuPrincipal();
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Gestor de tareas
+Creé un programa de línea de comandos que permita al usuario gestionar una lista de tareas. El programa utiliza la biblioteca inquirer para interactuar con el usuario y recopilar la información necesaria.
+
+El programa ofrece las siguientes funcionalidades:
+
+1.Mostrar la lista de tareas: El usuario podrá ver todas las tareas existentes en la lista.
+
+2.Agregar una nueva tarea: El usuario podrá agregar una nueva tarea a la lista. La tarea deberá tener un nombre y una descripción.
+
+3.Marcar una tarea como completada: El usuario podrá marcar una tarea como completada, seleccionándola de la lista de tareas existentes.
+
+4.Eliminar una tarea: El usuario podrá eliminar una tarea de la lista, seleccionándola de la lista de tareas existentes.
+
+El programa muestra un menú al usuario con estas opciones y le permite seleccionar la opción deseada. Después de cada acción realizada, el programa muestra la lista actualizada de tareas.
